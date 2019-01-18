6707746c608992d61749e8eb68afdfa5aef4d643
PorkPies1 <46811427+PorkPies1@users.noreply.github.com>
2019-01-18 10:21:58 +0000

    edit buypage

--- Reading-CS/Reading-CS/Buypage.cs
+++ Reading-CS/Reading-CS/Buypage.cs
@@ -16,5 +16,15 @@ namespace Reading_CS
         {
             InitializeComponent();
         }
+
+        private void Buypage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
