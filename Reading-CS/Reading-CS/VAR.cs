50a73f1888d928954ce8d5b1be4e6d0a11e23084
PorkPies1 <46811427+PorkPies1@users.noreply.github.com>
2019-01-22 13:04:05 +0000

    var made

---
+++ Reading-CS/Reading-CS/VAR.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reading_CS
+{
+    class VAR
+    {
+        public static bool sunday = false;
+    }
+}
