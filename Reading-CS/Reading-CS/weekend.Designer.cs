16e6204559e6cd566269aef7a6316f0672b13337
PorkPies1 <46811427+PorkPies1@users.noreply.github.com>
2019-01-24 09:17:09 +0000

    change things

--- Reading-CS/Reading-CS/weekend.Designer.cs
+++ Reading-CS/Reading-CS/weekend.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // weekend
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(22)))), ((int)(((byte)(28)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(203)))), ((int)(((byte)(5)))));
+            this.Name = "weekend";
             this.Text = "weekend";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
